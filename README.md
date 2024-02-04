f2ce27ea1d0d9a741661c3a1346d560f04d482aa
Nick82285 <willeford_luxuary@aleeas.com>
2024-02-03 18:30:16 -0800

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+<<<<<<< HEAD
+This is my repository for gentoo system update, editing, building and installing the kernel.
+=======
+# Gentoo Management & Deployment scripts
+>>>>>>> 7d66529 (Add 'install.sh' and reorganize project)
+# gentoo-management-scripts
---
+++ install.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -xe
+
+files=("ekernel" "mkkernel" "systemupdate")
+
+source ./gentoo-management-scripts/utils/suidrequired
+
+mkdir -p /usr/local/libexec/
+cp -r ./gentoo-management-scripts /usr/local/libexec/
+
+for file in "${files[@]}"; do
+	cp "$file" /usr/local/bin/
+	chmod 0755 "/usr/local/bin/$file"
+	chown root:root "/usr/local/bin/$file"
+done
